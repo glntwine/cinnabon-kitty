7aaf5ff0bef790049546b4ca232faf4f63cad79b
glntwine <peresetsky.egor@gmail.com>
2024-02-17 19:11:57 +0400

    Added a placeholder sprite for kitty. Implemented walk animation.

---
+++ game/levels/templates/point_and_click/InteractionMenu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if (Input.is_action_pressed("LMB") && !visible):
+		visible = true
+	
+	if (Input.is_action_just_released("LMB")):
+		
+		visible = false
+	
--- game/levels/templates/point_and_click/Player.gd
+++ game/levels/templates/point_and_click/Player.gd
@@ -6,11 +6,13 @@ extends CharacterBody2D
 @onready var navigation_region = $"../Environment/NavigationRegion2D"
 var navigation_target: Vector2
 
+# This code is copy&paste code from doc. Basically a proof of concept.
 func _ready():
 	# These values need to be adjusted for the actor's speed
 	# and the navigation layout.
 	navigation_agent.path_desired_distance = 4.0
 	navigation_agent.target_desired_distance = 4.0
+	$AnimatedSprite2D.play("idle")
 
 func actor_setup():
 	# Wait for the first physics frame so the NavigationServer can sync.
@@ -23,17 +25,23 @@ func set_movement_target(movement_target: Vector2):
 	navigation_agent.target_position = movement_target
 
 func _physics_process(delta):
-	if (Input.is_action_pressed("LMB_Click")):
+	if (Input.is_action_pressed("LMB")):
 		navigation_target = get_viewport().get_mouse_position()
 		navigation_agent.target_position = navigation_target
+		
+		# flip a sprite if target is the other way
+		var flip = position.direction_to(navigation_target).x < 0
+		$AnimatedSprite2D.flip_h = flip
+		
+		$AnimatedSprite2D.play("walk")
 	
 	if navigation_agent.is_navigation_finished():
+		$AnimatedSprite2D.play("idle")
 		return
 	
 	var current_agent_position: Vector2 = global_position
 	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
 
 	velocity = current_agent_position.direction_to(next_path_position) * player_speed * delta
-	print(velocity)
 	move_and_collide(velocity)
 	
